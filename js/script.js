b7e46947f0025d43f7e107a19699458dbb8d2815
melodramaticbxtch <kiarabgamarra@gmail.com>
2025-09-30 16:11:37 -0300

    Versión final

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
+//mensaje de hora
+let hora = new Date().getHours();
+let mensaje;
+
+if (hora < 12) {
+    mensaje = "¡Buenos días!";
+} else if (hora < 18) {
+    mensaje = "¡Buenas tardes!";
+} else {
+    mensaje = "¡Buenas noches!";
+}
+
+alert(mensaje);
 // Saludo personalizado
 let nombreUsuario = prompt("¡Bienvenido! ¿Cuál es tu nombre?");
 if (nombreUsuario) {
   document.querySelector("header h3").textContent += nombreUsuario + ", Bienvenid@!";
 }
-
+// buscador
 document.getElementById("btnBusqueda").addEventListener("click", () => {
   let busqueda = document.getElementById("inputBusqueda").value.toLowerCase();
 
